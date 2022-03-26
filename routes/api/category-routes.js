3d97292e42984cdc15e6345c29a8d62317ca5c60
Sami Fares <djsamif@gmail.com>
2022-03-26 12:24:06 -0400

    changes to server.js file and trouble shooting the routes with console log

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -6,23 +6,28 @@ const { Category, Product } = require('../../models');
 router.get('/', (req, res) => {
   // find all categories
   // be sure to include its associated Products
+  res.json({message:"this is the get all"})
 });
 
 router.get('/:id', (req, res) => {
   // find one category by its `id` value
   // be sure to include its associated Products
+  res.json({message:req.params.id})
 });
 
 router.post('/', (req, res) => {
   // create a new category
+ res.json(req.body)
 });
 
 router.put('/:id', (req, res) => {
   // update a category by its `id` value
+  res.json({message:"put"})
 });
 
 router.delete('/:id', (req, res) => {
   // delete a category by its `id` value
+  res.json({message:"delete"})
 });
 
 module.exports = router;
